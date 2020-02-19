a1273e1e60323bed5c122e32455b5463ee57d481
Lauren Hash <lah5175@hotmail.com>
2020-02-19 16:53:13 -0500

    jumpman finished

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,23 @@
 
 // Complete this algo
 const minJumps = arr => {
+  let currentIdx = 0;
+  let jumps = 0;
 
+  while (currentIdx < arr.length-1){
+    let value = arr[currentIdx];
+
+    if (currentIdx + value >= arr.length-1){
+      return jumps+1;
+    }
+
+    const steps = arr.slice(currentIdx+1, currentIdx + value + 1);
+    const maxVal = Math.max.apply(null, steps);
+    const stepIdx = steps.indexOf(maxVal) + currentIdx + 1;
+
+    jumps++;
+    currentIdx = stepIdx;
+  }
 };
 
 module.exports = minJumps
