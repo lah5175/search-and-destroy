57e9eae74fa0dc451defaa9bfe382042ec5e4bd9
Ayse Erduran <ae2456@columbia.edu>
2020-02-19 15:54:26 -0500

    completed binary-search with 0(1) space

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,27 +1,27 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	const sorted = array.sort((a, b) => a - b);
-	const midpoint = Math.floor(sorted.length/2);
-	if (sorted.length <= 1){
-		if(sorted[0] === target){
-			return true;
-		} else {
-			return false;
-		}
-	}
-	let slicedArr = []
-	if (sorted[midpoint] === target){
-		return true;
-	} else if (target > sorted[midpoint]){
-		slicedArr = sorted.slice(midpoint)
-	} else {
-		slicedArr = sorted.slice(0, midpoint);
-	}
-	console.log(slicedArr);
-	return binarySearch(slicedArr, target);
-};
+// const binarySearch = (array, target) => {
+// 	const sorted = array.sort((a, b) => a - b);
+// 	const midpoint = Math.floor(sorted.length/2);
+// 	if (sorted.length <= 1){
+// 		if(sorted[0] === target){
+// 			return true;
+// 		} else {
+// 			return false;
+// 		}
+// 	}
+// 	let slicedArr = []
+// 	if (sorted[midpoint] === target){
+// 		return true;
+// 	} else if (target > sorted[midpoint]){
+// 		slicedArr = sorted.slice(midpoint)
+// 	} else {
+// 		slicedArr = sorted.slice(0, midpoint);
+// 	}
+// 	console.log(slicedArr);
+// 	return binarySearch(slicedArr, target);
+// };
 
 /*
 	EXTRA CREDIT:
@@ -31,5 +31,24 @@ const binarySearch = (array, target) => {
 	How can we use other pieces of data, like pointers, to avoid slicing?
 
 */
+
+
+const binarySearch = (array, target) => {
+	let upper = array.length;
+	let lower = 0;
+	while(upper - lower > 1){
+		let midpoint = Math.floor((upper - lower) / 2) + lower;
+		console.log('midpoint', midpoint);
+		if(array[midpoint] === target){
+			return true;
+		} else if (target > array[midpoint]){
+			lower = midpoint;
+		} else {
+			upper = midpoint;
+		}
+	}
+	if(array[upper] === target || array[lower] === target) return true;
+	return false;
+}
 
 module.exports = binarySearch
