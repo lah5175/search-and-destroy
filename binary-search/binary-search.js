af72d3eb9dfc2d59f5db1b7fddab108ed21ad481
Ayse Erduran <ae2456@columbia.edu>
2020-02-19 15:14:26 -0500

    completed binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,25 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	const sorted = array.sort((a, b) => a - b);
+	const midpoint = Math.floor(sorted.length/2);
+	if (sorted.length <= 1){
+		if(sorted[0] === target){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	let slicedArr = []
+	if (sorted[midpoint] === target){
+		return true;
+	} else if (target > sorted[midpoint]){
+		slicedArr = sorted.slice(midpoint)
+	} else {
+		slicedArr = sorted.slice(0, midpoint);
+	}
+	console.log(slicedArr);
+	return binarySearch(slicedArr, target);
 };
 
 /*
